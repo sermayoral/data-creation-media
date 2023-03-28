a38340d1f8d8612beda936aecfee3ba8f29b1b5b
Sergio Mayoral Martínez <sergiomayoralmartinez@gmail.com>
2023-03-28 23:56:42 +0200

    Se añade opcion a mover los ficheros que no disponen de fecha de creacion

--- data-creation-media.py
+++ data-creation-media.py
@@ -14,72 +14,77 @@ videoFormats = ['mp4']
 
 logger = logging.getLogger('data-creation-media')
 
-def process(path, year_from):
-  for fichero in os.listdir(path):
-    filename = os.fsdecode(fichero)
-    filepath = os.path.join(path, fichero)
-    if os.path.isdir(filepath):
-      process(filepath, year_from)
-      continue
-    nombre_ext = filename.split('.')
-    if nombre_ext[-1].lower() == 'py' or filename == '.syncmetadata':
-      continue
-    valid_media = 0
-    if nombre_ext[-1].lower() in imgFormats:
-      try:
-        im = Image.open(filepath)
-        exif = im._getexif()
-        im.close()
-        if DATE_TIME_ORIG_TAG in exif:
-          if not exif[DATE_TIME_ORIG_TAG]:
-            valid_media = 2
-            logger.info('La fecha de captura de la imagen no esta definida')
-          elif int(exif[DATE_TIME_ORIG_TAG][0:4]) < year_from:
-            valid_media = 2
-            logger.info('Imagen tomada antes del año minimo aceptado')
-        else:
-          valid_media = 2
-          logger.info('No se encuentra la fecha de captura en la imagen')
-      except Exception as err:
-        valid_media = 3
-        logger.error(err)
-        
-    elif nombre_ext[-1].lower() in videoFormats:
-      parser = createParser(filepath)
-      if not parser:
-        valid_media = 3
-        logger.info('Error de parseo de video')
+def process(path, year_from, path_move_media):
+  if not path_move_media or os.path.normpath(path) != os.path.normpath(path_move_media):
+    for fichero in os.listdir(path):
+      filename = os.fsdecode(fichero)
+      filepath = os.path.join(path, fichero)
+      if os.path.isdir(filepath):
+        process(filepath, year_from, path_move_media)
         continue
-      with parser:
+      nombre_ext = filename.split('.')
+      if nombre_ext[-1].lower() == 'py' or filename == '.syncmetadata':
+        continue
+      valid_media = 0
+      if nombre_ext[-1].lower() in imgFormats:
         try:
-          metadata = extractMetadata(parser)
+          im = Image.open(filepath)
+          exif = im._getexif()
+          im.close()
+          if DATE_TIME_ORIG_TAG in exif:
+            if not exif[DATE_TIME_ORIG_TAG]:
+              valid_media = 2
+              logger.info('La fecha de captura de la imagen no esta definida')
+            elif int(exif[DATE_TIME_ORIG_TAG][0:4]) < year_from:
+              valid_media = 2
+              logger.info('Imagen tomada antes del año minimo aceptado')
+          else:
+            valid_media = 2
+            logger.info('No se encuentra la fecha de captura en la imagen')
         except Exception as err:
-          metadata = None
+          valid_media = 3
           logger.error(err)
-      if not metadata:
-        valid_media = 3
-        logger.info('Error al extraer metadatos de video')
-        continue
-      fecha_creacion_video = None;
-      for line in metadata.exportPlaintext():
-        if line.split(':')[0] == '- Creation date':
-          fecha_creacion_video = line.split(':')[1].split()[0]
+      elif nombre_ext[-1].lower() in videoFormats:
+        parser = createParser(filepath)
+        if not parser:
+          valid_media = 3
+          logger.info('Error de parseo de video')
           continue
-      if fecha_creacion_video and int(fecha_creacion_video[0:4]) < year_from:
-        fecha_creacion_video = None
-      if not fecha_creacion_video:
-        valid_media = 2
-        logger.info('No se encuentra la fecha de captura en el video, o su año de captura es menor al aceptado')
-    else:
-      valid_media = 1
-    # Procesamos los errores
-    if valid_media > 0:
-      if valid_media == 1:
-        logger.warning('El archivo %s no es un medio aceptado', filepath)
-      elif valid_media == 2:
-        logger.warning('El archivo %s no contiene el metadato de fecha de creacion', filepath)
-      elif valid_media == 3:
-        logger.warning('No se pueden extraer metadatos del archivo %s', filepath)
+        with parser:
+          try:
+            metadata = extractMetadata(parser)
+          except Exception as err:
+            metadata = None
+            logger.error(err)
+        if not metadata:
+          valid_media = 3
+          logger.info('Error al extraer metadatos de video')
+          continue
+        fecha_creacion_video = None;
+        for line in metadata.exportPlaintext():
+          if line.split(':')[0] == '- Creation date':
+            fecha_creacion_video = line.split(':')[1].split()[0]
+            continue
+        if fecha_creacion_video and int(fecha_creacion_video[0:4]) < year_from:
+          fecha_creacion_video = None
+        if not fecha_creacion_video:
+          valid_media = 2
+          logger.info('No se encuentra la fecha de captura en el video, o su año de captura es menor al aceptado')
+      else:
+        valid_media = 1
+      # Procesamos los errores
+      if valid_media > 0:
+        if valid_media == 1:
+          logger.warning('El archivo %s no es un medio aceptado', filepath)
+        elif valid_media == 2:
+          logger.warning('El archivo %s no contiene el metadato de fecha de creacion', filepath)
+        elif valid_media == 3:
+          logger.warning('No se pueden extraer metadatos del archivo %s', filepath)
+        # En caso de que se desee mover el fichero a un path
+        if path_move_media:
+          if not os.path.exists(path_move_media):
+            os.makedirs(path_move_media)
+          os.rename(filepath, os.path.join(path_move_media, fichero))
 
 def parseArguments():
   # Create argument parser
@@ -92,6 +97,7 @@ def parseArguments():
 
   # Optional arguments
   parser.add_argument("-y", "--year-from", help="Minimum acceptable media year (default 2000)", type=int, default=2000)
+  parser.add_argument("-m", "--move-media", help="Path to move media files without creation date", type=str, default=None)
   parser.add_argument("-l", "--log-level", help="Set log level", type=str, default='WARNING')
 
   # Print version
@@ -106,5 +112,5 @@ if __name__ == '__main__':
   # Set logging
   logging.basicConfig(level=args.log_level.upper())
   # Process media
-  process('.', args.year_from)
+  process('.', args.year_from, args.move_media)
 
